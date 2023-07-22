4209eb2de1f5a1e32d4723a9003923c8f2eac9d3
Jeyalakshmi Manivasagam <110182064+jeya91296@users.noreply.github.com>
2023-07-22 22:26:42 +0530

    creating own java class myutils method
    check class MethodsInOwnClass

---
+++ MyUtils.java
@@ -0,0 +1,32 @@
+package java8project;
+
+public class MyUtils {
+		public static void junk(String ar)
+	{
+		System.out.println("Something " + ar);
+	}
+    		
+	public static void junk(int ar) 
+	{
+		System.out.println("Integer passed in " + ar);
+	}
+	
+	public static void sum2num(int a,int b)//variable these are parameters
+	{
+		System.out.println(a+b);
+	}
+	
+	//return type int
+	public static int add10(int c)
+	{
+		int result=c+10;
+		return result;
+	}
+	
+	//return type string
+	public static String  jun(String ar)
+	{
+		return ar;
+	}
+	
+}
